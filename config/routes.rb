1b00b33e8d8a2c82e8166d6854fb072b0b7b6485
balboni <brian@brianbalboni.com>
2015-08-18 20:39:57 -0500

    not working 3.2.0

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,7 @@ Rails.application.routes.draw do
     scope module: :v1, 
               constraints: ApiConstraints.new(version: 1, default: true) do
                 # we are going to list our constraints here
+                resources :users, :only => [:show]
     end
-
   end
-
 end
